70a3f398771fc315fb7b423638c9ff449a08efc7
Lucy Wang <lw1582@nyu.edu>
2014-12-05 15:09:56 -0500

    add user input interface

---
+++ Project/scripts/geoCoding.py
@@ -0,0 +1,23 @@
+'''
+December 4, 2014
+
+@author: lucy wang
+'''
+
+from pygeocoder import Geocoder
+from pygeolib import GeocoderError
+import __builtin__
+
+def user_location(address):
+  state_city = set(['new york','New York','ny','NY','Ny','New york','new York'])
+  if __builtin__.any(keyword in address for keyword in state_city):
+    address_state = Geocoder.geocode(address)
+  else:
+    address_state = Geocoder.geocode(address = "New York")
+  
+  return address_state.formatted_address
+
+def get_coordinates(address):
+  address = Geocoder.geocode(address)
+  return address.coordinates
+
---
+++ Project/scripts/userInput.py
@@ -0,0 +1,52 @@
+'''
+Dec 1 2014
+
+@author: lucy wang
+
+'''
+import re
+import sys
+from pygeocoder import Geocoder
+from pygeolib import GeocoderError
+from geoCoding import user_location, get_coordinates
+from customExceptions import EmptyStringException
+
+def get_manual_input():
+  '''
+  this function takes an input from the user
+  '''
+  address_input = raw_input('Address to search: ')
+  if address_input == "":
+    raise EmptyStringException
+
+  return address_input
+
+def convert_address(address_input):
+  '''
+  this function converts an inputed address into a geocoder formatted address and coordinates
+
+  user_location and get_coordinates are functions built in the geoCoding module
+  '''
+  validate_address(address_input)
+  address = user_location(address_input)
+  latitude, longitude = get_coordinates(address)
+
+  return address, latitude, longitude
+  
+def validate_address(address):
+  '''
+  this function checks whether the user input is a valid address
+  '''
+  try:
+    address = Geocoder.geocode(address)
+  except GeocoderError:
+    print "The address entered cannot be found"
+    sys.exit(1)
+
+if __name__ == '__main__':
+  address = get_manual_input()
+  address, lat, lon = convert_address(address)
+  print "address is: " + str(address)
+  print "lat: " + str(lat)
+  print "long: " + str(lon)
+    

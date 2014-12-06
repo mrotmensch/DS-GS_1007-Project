ccc0a0c0ae97e7bdd4a35c7cd4b218e725034724
Lucy Wang <lw1582@nyu.edu>
2014-12-05 21:03:21 -0500

    main and userOutput.py

--- Project/main.py
+++ Project/main.py
@@ -1,22 +1,26 @@
 from location_class import *
 from userInput import get_manual_input, convert_address
-#from userOutput import output_map
+from userOutput import output_map, get_output_coord
 import sys
 
 def main():
   address = get_manual_input()
   address= convert_address(address)
   
-  
   wifi = NearestWifi()
-  print "searching for \n" + address + '\n\n\n'
-  print wifi.search_results(address)
   
+  print "Searching for: \n" + address + '\n\n\n'
+  search_results = wifi.search_results(address)
+  print search_results
+  
+  output_map(wifi.long_,wifi.lat,search_results)
 
   while True:
     more = raw_input("Type Y for next 5 results or C to change address: ")
     if more.lower() == 'y':
-      print wifi.search_results(address)
+      search_results = wifi.search_results(address)
+      print search_results
+      output_map(wifi.long_,wifi.lat,search_results)
     elif more.lower() == 'c':
       main()
     else:
--- Project/userOutput.py
+++ Project/userOutput.py
@@ -3,31 +3,38 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pylot as plt
 import numpy as np
 
-def output_map(user_lat, user_long,lllong,lllat,urlong,urlat,wifi):
-  """ user_lat, user_long,
-  lllong: min of 5 long 
-  lllat:  min 5 lat 
-  urlong:
-  urlat:
-  wifi:
+def get_output_coord(search_results):
+  
+  max_lat = search_results.Lat.max()
+  min_lat = search_results.Lat.min()
+  max_lon = search_results.Long_.max()
+  min_lon = serach_results.Long_.min()
+  plot_results = search_results[['Lat','Long_','SSID']]
+
+  return max_lat, min_lat, max_lon, min_lon, plot_results
+
+def output_map(user_long,user_lat, search_results):
+  """ user_lat, user_long: lat and long coordinates of the user address
   """
+  
+  max_lat, min_lat, max_lon, min_lon, plot_results = get_output_coord(search_results)
+  
   pylab.rcParams['figure.figsize'] = (8.0,6.4)
 
-  llcrnrlon = lllong -1
-  llcrnrlat= lllat -1
-  urcrnrlon = urlong +1
-  urcrnrlat = urlat +1
+  max_lat = max_lat + 1
+  min_lat = min_lat - 1
+  max_lon = max_lon + 1
+  min_lat = min_lat - 1
 
-  map = Basemap(projection='merc', lat_0 = user_lat, lon_0 = user_long, resolution = 'h', area_thresh = 0.1 , llcrnrlon=llcrnrlon, llcrnrlat=llcrnrlat, urcrnrlon=urcrnrlon, urcrnrlat=urcrnrlat)
-   
+  map = Basemap('merc',  user_lat,  user_long, 'h', 0.1, min_long, min_lat, max_long, max_lat)
   map.drawcoastlines()
   map.drawcountries()
   map.fillcontinents(color = 'white')
   map.drawmapboundary()
 
-  lon = list(wifi[0])
-  lat = list(wifi[1])
-  label = list(wifi[2])
+  lon = list(plot_results.Long_)
+  lat = list(plot_results.Lat)
+  label = list(plot_results.SSID)
   x,y = map(lon, lat)
   map.plot(x, y, 'bo', markersize=11)
   

00b050b9cdc53dcae995089b254485744ad169d1
Maya Rotmensch <mayarotmensch@gmail.com>
2014-12-09 12:56:09 -0500

    erdf

--- Project/customExceptions/customExceptions.py
+++ Project/customExceptions/customExceptions.py
@@ -21,3 +21,6 @@ class InvalidInputException(Exception):
 class Address_not_valid(Exception):
     """This exeption is raised when the address the user provides can either not be converted or was found by the geocoder API to not be valid."""
     pass
+
+
+    
--- Project/main.py
+++ Project/main.py
@@ -1,40 +1,58 @@
 from Classes.location_class import *
 from userInput import get_manual_input, convert_address
 from userOutput import output_map, get_output_coord
+from geoCoding.mapOutput import mapOutput, mapImage,closeMap
 import sys
 
 def main():
 
-  try:
-    address = get_manual_input()
-    address= convert_address(address)
-  except Address_not_valid as n:
-    print n
+  while True:
+    try: 
+      address = get_manual_input()
+      address= convert_address(address)
+    
+      wifi = NearestWifi()
+    
+      print "Searching for: \n" + address + '\n\n'
+      search_results = wifi.search_results(address)
+      show_results = ['Name','Location','Location_T','SSID','Type','distance']
+      print search_results[show_results].reset_index(drop = True)
+      
+      
+      url = mapOutput(wifi.long_, wifi.lat,search_results)
+      image = mapImage(url)
+      image.show()
+      break
 
-  wifi = NearestWifi()
+    except UnreadableData as u:
+      print u
+
+    except (EmptyStringException, NotInNYException, GeocoderError, InvalidInputException, Address_not_valid) as e:
+      print "Please enter a valid address in NYC to search"
+
+    except KeyboardInterrupt as k :
+      print " \n you chose to terminate the program... goodbye!"
+      sys.exit()
   
-  print "Searching for: \n" + address + '\n\n\n'
-  search_results = wifi.search_results(address)
-  print search_results
 
   while True:
-    try:
-      more = raw_input("Type Y for next 5 results or C to change address: ")
-      if more.lower() == 'y':
-        search_results = wifi.search_results(address)
-        print search_results
-
-      elif more.lower() == 'c':
-        main()
-      else:
-        sys.exit()
-    except Address_not_valid as n:
-      print n
-
+    more = raw_input("Type Y for next 5 results or C to change address: ")
+    if more.lower() == 'y':
+      closeMap()
+      search_results = wifi.search_results(address)
+      print search_results[show_results]#.reset_index(drop = True)
+      
+      '''url = mapOutput(wifi.long_,wifi.lat,search_results)
+      image = mapImage(url)'''
+      
+    elif more.lower() == 'c':
+      main()
+    else:
+      print " \n you chose to terminate the program... goodbye!"
+      sys.exit()
   
 
 if __name__ == '__main__':
-  try:
-    main()
-  except KeyboardInterrupt:
-    sys.exit()
+  #try:
+  main()
+  

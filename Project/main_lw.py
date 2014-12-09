e9df49e90a1c54fc98a16654b1635f4aaff45762
Maya Rotmensch <mayarotmensch@gmail.com>
2014-12-09 12:51:13 -0500

    excpetion catching in place

--- Project/main_lw.py
+++ Project/main_lw.py
@@ -5,26 +5,40 @@ from geoCoding.mapOutput import mapOutput, mapImage,closeMap
 import sys
 
 def main():
-  address = get_manual_input()
-  address= convert_address(address)
-  
-  wifi = NearestWifi()
-  
-  print "Searching for: \n" + address + '\n\n'
-  search_results = wifi.search_results(address)
-  show_results = ['Name','Location','Location_T','SSID','Type','distance']
-  print search_results[show_results].reset_index(drop = True)
-  
-  '''
-  url = mapOutput(wifi.long_, wifi.lat,search_results)
-  image = mapImage(url)
-  image.show()'''
+
+  while True:
+    try: 
+      address = get_manual_input()
+      address= convert_address(address)
+    
+      wifi = NearestWifi()
+    
+      print "Searching for: \n" + address + '\n\n'
+      search_results = wifi.search_results(address)
+      show_results = ['Name','Location','Location_T','SSID','Type','distance']
+      print search_results[show_results].reset_index(drop = True)
+      
+      
+      url = mapOutput(wifi.long_, wifi.lat,search_results)
+      image = mapImage(url)
+      image.show()
+      break
+
+    except UnreadableData as u:
+      print u
+
+    except (EmptyStringException, NotInNYException, GeocoderError, InvalidInputException, Address_not_valid) as e:
+      print "Please enter a valid address in NYC to search"
+
+    except KeyboardInterrupt as k :
+      print " \n you chose to terminate the program... goodbye!"
+      sys.exit()
   
 
   while True:
     more = raw_input("Type Y for next 5 results or C to change address: ")
     if more.lower() == 'y':
-      #closeMap()
+      closeMap()
       search_results = wifi.search_results(address)
       print search_results[show_results]#.reset_index(drop = True)
       
@@ -34,12 +48,11 @@ def main():
     elif more.lower() == 'c':
       main()
     else:
+      print " \n you chose to terminate the program... goodbye!"
       sys.exit()
   
 
 if __name__ == '__main__':
-  try:
-    main()
-  except KeyboardInterrupt:
-    print " \n you chose to terminate the program... goodbye!"
-    sys.exit()
+  #try:
+  main()
+  

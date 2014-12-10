591cc71d13db43a35cb4c47e7795e35b76b36c8e
Maya Rotmensch <mayarotmensch@gmail.com>
2014-12-10 15:09:43 -0500

    update heatmap

--- Project/heatmap.py
+++ Project/heatmap.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from textwrap import *
 from Classes.location_class import *
 from userInput import get_manual_input, convert_address
+
+
+
 def heat_map(DF):
     '''
     TODO
@@ -16,34 +20,32 @@ def heat_map(DF):
 
 
     map_data_trans = map_data.T
+
+
+
     fig, ax = plt.subplots()
     heatmap = ax.pcolor(map_data_trans, cmap=plt.cm.Blues)
 
     column_labels = map_data_trans.columns
     row_labels = map_data_trans.index
-
+    row_labels = ['\n'.join(wrap(l.lower(),15)) for l in row_labels]
     print map_data_trans.shape
 
     # want a more natural, table-like display
     ax.invert_yaxis()
     ax.xaxis.tick_top()
 
-    #ax.set_yticks(rotation = 35)
-
     #put the major ticks at the middle of each cell
     ax.set_xticks(np.arange(map_data_trans.shape[1])+0.5, minor=False)
     ax.set_yticks(np.arange(map_data_trans.shape[0])+0.5, minor=False)
 
-    ax.set_xticklabels(column_labels, minor=False)
-    y = ax.set_yticklabels(row_labels, minor=False)
-    ax.labelsize(8)
+    ax.set_xticklabels(column_labels, minor=False, fontsize  = 7)
+    y = ax.set_yticklabels(row_labels, minor=False, fontsize = 7)
 
+    fig.set_size_inches(7,9)
 
-    #plt.setp(y, rotation=35)
-    #ax.set_aspect(0.5)
-    
-    #plt.tight_layout()
-    plt.show()
+  
+    plt.savefig("heatmap.pdf")
 
 
 #http://stackoverflow.com/questions/14391959/heatmap-in-matplotlib-with-pcolor.
